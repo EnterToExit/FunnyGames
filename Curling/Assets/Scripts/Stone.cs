ffd97cfc90a94cc3ed695647254bea3c31e1d8ae
EnterToExit <osmiumsdk@gmail.com>
2023-02-20 23:48:02 +0300

    camera flex update

--- Curling/Assets/Scripts/Stone.cs
+++ Curling/Assets/Scripts/Stone.cs
@@ -3,10 +3,28 @@ using UnityEngine;
 
 public class Stone : MonoBehaviour
 {
-    
+    private float _passedTime;
+    private CameraController _cameraController;
+    private bool _dead;
+
     private void Awake()
     {
+        _cameraController = FindObjectOfType<CameraController>();
         var shootingService = FindObjectOfType<ShootingService>();
         shootingService._stones.Add(gameObject);
+    }
+
+    private void Update()
+    {
+        var speed = gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+        if (speed == 0f && !_dead)
+        {
+            _passedTime += Time.deltaTime;
+        }
+
+        if (!(_passedTime > 1f)) return;
+        _dead = true;
+        _passedTime = 0f;
+        _cameraController.ResetCameraTarget();
     }
 }
