cf18c6536cec4b127fb5e5de18ba9c7696b2bc73
EnterToExit <osmiumsdk@gmail.com>
2023-03-18 00:05:16 +0300

    overlapsphere

--- NigPong/Assets/Scripts/BallController.cs
+++ NigPong/Assets/Scripts/BallController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using static System.Linq.Enumerable;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
@@ -11,20 +12,34 @@ public class BallController : MonoBehaviour
 
     private void Start()
     {
-        var randomX = new System.Random().Next(-10, 10);
-        var randomY = new System.Random().Next(-10, 10);
-        _ballDirection = new Vector3(randomX, randomY, 0).normalized;
+        // var randomX = new System.Random().Next(-10, 10);
+        // var randomY = new System.Random().Next(-10, 10);
+        // _ballDirection = new Vector3(randomX, randomY, 0).normalized;
+        _ballDirection = new Vector3(5f, 0f, 0f).normalized;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        UpdateBallPos();
+        MoveBall();
+        if (IsTouching()) ChangeDireciton();
     }
 
-    private void UpdateBallPos()
+    private void MoveBall()
     {
         var currentBallPos = _ballPos.position;
         var newBallPos = currentBallPos + _ballDirection * _ballSpeed;
         _ballPos.position = newBallPos;
+    }
+
+    private bool IsTouching()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(_ballPos.position, 1f);
+        if (hitColliders.Count() > 1) return true;
+        else return false;
+    }
+
+    private void ChangeDireciton() 
+    {
+        _ballDirection = _ballDirection * -1f;
     }
 }
